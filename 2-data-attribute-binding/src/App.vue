3040e3baedf4fb4cfa6a7f8e1ac027d96e5a94bc
Eswaran-Rahul-Krishna <eswaran.krishnamurthy@outlook.com>
2023-02-22 15:55:43 +0000

    v-bind

---
+++ 2-data-attribute-binding/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>Bind data to attribute</h1>
+
+  <h3>Syntax  =  ( v-bind:attr="dataProperty" ) or  ( :attr="dataProperty" )</h3>
+
+  <p>click here (v-bind:href="link") - <a v-bind:href="link">google</a></p>
+
+  <p>click here (:href="link") - <a :href="link">google</a></p>
+</template>
+
+
+<script>
+export default{
+  data(){
+    return {
+      link:'https://www.google.co.uk/'
+    }
+  }
+}
+
+</script>
