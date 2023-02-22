2f8e50c50796ea3821a19562f54ede0a77180c25
Eswaran-Rahul-Krishna <eswaran.krishnamurthy@outlook.com>
2023-02-22 16:15:59 +0000

    3-data and methods

---
+++ 3-data-and-methods/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <h1>{{ myHeader() }}</h1>
+
+  <h3>{{ body('Im parameter value') }}</h3>
+
+  <h3>{{ bodyDataValue() }}</h3>
+
+  <h1>{{footer()}}</h1>
+</template>
+
+
+<script>
+export default{
+  data(){
+    return{
+      dataValue:'Im Data Value'
+    }
+  },
+methods:{
+  myHeader(){
+    return 'This is header'
+  },
+  body(name){
+    return name;
+  },
+  bodyDataValue(){
+    return this.dataValue;
+  },
+  footer(){
+    return 'This is footer'
+  }
+}
+}
+</script>
