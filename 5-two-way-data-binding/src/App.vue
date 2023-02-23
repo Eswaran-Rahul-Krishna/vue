944d266de4bc1daaff3ead570ab40bcf7eca396c
Eswaran-Rahul-Krishna <eswaran.krishnamurthy@outlook.com>
2023-02-23 14:03:51 +0000

    5-2-way-data-binding

---
+++ 5-two-way-data-binding/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <h5>two-way data binding using => v-model="data"</h5>
+  <label>Text Value</label>
+  <input type="text" v-model="textValue"><br/><br/>
+  <button @click="reset">Reset textValue</button><br/><br/>
+  <label>Binded text value : </label>
+  <h3>{{ textValue }}</h3>
+</template>
+
+
+<script>
+export default{
+  data(){
+    return{
+      textValue:''
+    }
+  },
+  methods:
+  {
+    reset(){
+      this.textValue ='';
+    }
+  }
+}
+</script>
+
+
