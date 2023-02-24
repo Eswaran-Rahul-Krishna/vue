0c1485a2fc7a7165c8e87542abed1712e075eea1
Eswaran-Rahul-Krishna <eswaran.krishnamurthy@outlook.com>
2023-02-24 10:48:39 +0000

    computed added

---
+++ 7-computed-property/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <h1>Drawback of method.  </h1><br/>
+
+  <h3>click add button and see the console log  </h3>
+
+
+  <h3> count  =  {{ count }}</h3><br/><br/>
+<button @click="add">Add</button> <br/><br/>
+
+<input type="text" @keyup="appendInputText"/>
+
+<h2>{{ formatedInputValue }}</h2>
+
+</template>
+
+
+<script>
+export default{
+  data(){
+    return{
+      count:0,
+      inputText:''
+    }
+  },
+  methods:{
+    appendInputText(event){
+       this.inputText = event.target.value;
+    },
+    add(){
+      this.count++;
+    }   
+  },
+  computed:{
+    formatedInputValue()
+    {
+      console.log('executed when only input is changed');
+      return '==>'+this.inputText 
+    }
+  }
+}
+
+</script>
