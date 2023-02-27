d220a5789a4865579fbb412f1da97b6614182456
Eswaran-Rahul-Krishna <eswaran.krishnamurthy@outlook.com>
2023-02-27 11:07:46 +0000

    component-intro

---
+++ 2-components/1-components-intro/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <new-component></new-component>
+
+  <new-component></new-component>
+
+  <new-component></new-component>
+
+  <new-component></new-component>
+
+  <new-component></new-component>
+</template>
+
+
+<script>
+
+import newComponent from './components/new-component.vue'
+
+export default{
+  components:{
+    newComponent
+  }
+}
+
+</script>
---
+++ 2-components/1-components-intro/src/components/new-component.vue
@@ -0,0 +1,11 @@
+<template>
+    <h5 >my new component</h5>
+</template>
+
+<style>
+h5{
+    border: solid;
+width: 150px;
+height: 50px;
+}
+</style>
